ee7604d9c7726460d6d5ca800639a91e2cbaf662
suraj807 <surajdev805@gmail.com>
2022-05-25 16:05:31 +0530

    user registration

---
+++ REGEX-User-Registration-Problem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REGEX_User_Registration_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Regular Expression");
+            Console.WriteLine("Enter your first name");
+            string FirstName = Console.ReadLine(); // Creating user input for first name
+            Validation.FirstNameValidation(FirstName);////Calling method to validate firstname
+
+        }
+    }
+}
---
+++ REGEX-User-Registration-Problem/Validation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace REGEX_User_Registration_Problem
+{
+    internal class Validation
+    {
+
+        public static void FirstNameValidation(string FirstName)
+        {
+            string pattern = "^[A-Z][a-z]{2,}$"; // Creating REGEX pattern
+            if (Regex.IsMatch(FirstName, pattern)) //For checking regex are valid or invalid
+            {
+                Console.WriteLine("The First name is : " + FirstName);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name : " + FirstName);
+            }
+        }
+    }
+}
