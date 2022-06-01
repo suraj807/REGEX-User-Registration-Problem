6c566a3bfa7c4a88800d86bc84154b503eef9bc2
suraj807 <surajdev805@gmail.com>
2022-06-01 20:02:46 +0530

    EmailValidation

--- REGEX-User-Registration-Problem/Program.cs
+++ REGEX-User-Registration-Problem/Program.cs
@@ -10,12 +10,32 @@ namespace REGEX_User_Registration_Problem
     {
         static void Main(string[] args)
         {
+        //Console.WriteLine("\nWelcome to C# Regular Expression");
+        //User First name validation
+        Firstname:
+            Console.WriteLine("\nNote : Your first name should start with Capital Letter only and has minimum 3 Character and no space between letters");
+            Console.WriteLine("\n\nEnter your first name : ");
+            string fName = Console.ReadLine(); //storing first name entered by user in variable
+            bool checkFirstName = Validation.FirstNameValidation(fName); //Calling method to check first name is valid or not with argument
+            if (!checkFirstName)//If not valid then ask user to enter first name again
+                goto Firstname;
+            //User Last name validation
+            Lastname:
+            Console.WriteLine("\nNote : Your Last name should start with Capital Letter only and has minimum 3 Character and no space between letters");
+            Console.WriteLine("\n\nEnter your last name : ");
+            string lName = Console.ReadLine(); //storing last name entered by user in variable
+            bool checkLastName = Validation.LastNameValidation(lName); //Calling method to check last name is valid or not with argument
+            if (!checkLastName)//If not valid then ask user to enter last name again
+                goto Lastname;
+            //User Email Validation
+            Email:
+            Console.WriteLine("\n\nEnter your email name : ");
+            string email = Console.ReadLine(); //storing email address entered by user in variable
+            bool checkEmail = Validation.EmailValidation(email); //Calling method to check email is valid or not with argument
+            if (!checkEmail)//If not valid then ask user to enter email again
+                goto Email;
 
-            Console.WriteLine("Welcome to Regular Expression");
-            Console.WriteLine("Enter your first name");
-            string fName = Console.ReadLine(); // Creating user input for first name
-           Validation.FirstNameValidation(fName);//Calling method to validate firstname
-
+            Console.ReadLine();
         }
     }
 }
--- REGEX-User-Registration-Problem/Validation.cs
+++ REGEX-User-Registration-Problem/Validation.cs
@@ -9,7 +9,11 @@ namespace REGEX_User_Registration_Problem
 {
     internal class Validation
     {
-
+        /// <summary>
+        /// First Name Validation
+        /// </summary>
+        /// <param name="fName"></param>
+        /// <returns></returns>
         public static bool FirstNameValidation(string fName)
         {
             string pattern = "^[A-Z][a-z]{2,}$"; // Regex for first name validation
@@ -25,5 +29,47 @@ namespace REGEX_User_Registration_Problem
                 return false;
             }
         }
+        /// <summary>
+        /// Last Name Validation
+        /// </summary>
+        /// <param name="lName"></param>
+        /// <returns></returns>
+        public static bool LastNameValidation(string lName) // Creating a method for Last name validation
+        {
+            string pattern = "^[A-Z][a-z]{2,}$"; // Regex for last name validation
+            //If name entered by user is match with regex then it is valid otherwise not
+            if (Regex.IsMatch(lName, pattern))
+            {
+                Console.WriteLine($"\nYour last name \"{lName}\" is valid");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour last name \"{lName}\" is not valid");
+                Console.WriteLine("\nPlease follow naming convention of last name");
+                return false;
+            }
+        }
+        /// <summary>
+        /// Email Validation
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public static bool EmailValidation(string email)
+        {
+            string pattern = "^[0-9a-zA-Z]+[./+_-]{0,1}[0-9a-zA-Z]+[@][a-zA-Z0-9-]+[.][a-zA-Z]{2,}([.][a-zA-Z]{2,}){0,1}$"; // Regex for Email validation
+            //If email address entered by user is match with regex then it is valid otherwise not
+            if (Regex.IsMatch(email, pattern))
+            {
+                Console.WriteLine($"\nYour Email address \"{email}\" is valid");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour Email address \"{email}\" is not valid");
+                return false;
+            }
+        }
+
     }
 }
